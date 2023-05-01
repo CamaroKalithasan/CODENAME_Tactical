15925bf80e113ff8658f432cd75b824ecf670434
Camaro Kalithasan <CCKalithasan@student.fullsail.edu>
2023-05-01 16:01:05 -0400

    First Commit of Project

---
+++ CODENAMETactical/Source/CODENAMETactical/CODENAMETacticalGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CODENAMETacticalGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACODENAMETacticalGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACODENAMETacticalGameMode();
+};
+
+
+
